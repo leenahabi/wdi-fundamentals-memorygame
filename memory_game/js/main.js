672ed178e069c337c70f914254a1811c4bcdadac
leenahabi <leenaalnahabi@gmail.com>
2019-11-15 11:08:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+/*console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);*/
+const cards = ['queen','queen','king','king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+const cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+if (cardsInPlay.length === 2) {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+   alert("You found a match!");
+  }
+  else { alert("Sorry, try again.");}
+}
